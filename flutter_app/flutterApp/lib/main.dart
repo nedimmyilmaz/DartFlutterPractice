924d9b3c613fb36a5a30d9b4f7a8bc19d2d8e3a5
Nedim Yılmaz <ndmylmz591@yahoo.com>
2020-12-07 04:02:03 +0300

    ornek uygulama 7.12.20

--- flutter_app/flutterApp/lib/main.dart
+++ flutter_app/flutterApp/lib/main.dart
@@ -1,117 +1,87 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.purple,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+  runApp(
+    MaterialApp(
+        theme: ThemeData(
+            primarySwatch: Colors.lightGreen, accentColor: Colors.deepPurple),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "nedimyilmaz",
+              style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print("Butona tıklandı");
+            },
+            child: Icon(
+              Icons.emoji_people_sharp,
+              size: 30.0,
+              color: Colors.yellowAccent.shade700,
             ),
-          ],
+          ),
+          body: Container(
+            color: Colors.blue[200],
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment
+                  .spaceEvenly, // ikonların yeriyle ilgili widgetlar
+              /* crossAxisAlignment:
+                  CrossAxisAlignment.stretch,*/ // stretch ile gerdirir
+
+              children: [
+                Icon(Icons.ac_unit, color: Colors.yellowAccent, size: 55),
+
+                Icon(Icons.circle, color: Colors.deepPurple[200], size: 55),
+                Icon(Icons.add_circle_outline,
+                    color: Colors.indigoAccent[200], size: 55),
+                Icon(Icons.expand,
+                    color: Colors.pinkAccent,
+                    size: 55) // Expanded () ikonlar taşmasın diye ayarlar
+              ],
+            ),
+
+            /*height: 500,
+            width: 500,
+            alignment: Alignment.center,
+            // color: Colors.lightBlueAccent.shade100,
+            child: Text(
+              "Flutter'ı çok seviyorum.",
+              style: TextStyle(fontSize: 30.0),
+            ),
+
+            constraints: BoxConstraints.expand(width: 300, height: 400),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.yellow,
+                border: Border.all(color: Colors.purple)),
+            margin: EdgeInsets.only(left: 10, top: 40, right: 10, bottom: 40),
+            // margin vermeye yarar.
+
+            padding: EdgeInsets.all(80.0),*/
+            // minHeight: 100.0,
+            // maxHeight: 150.0,
+            // minWidth: 200.0,
+            // maxWidth: 250.0),
+          ),
+        )
+
+        /* body: Container(                              -
+          color: Colors.lightBlueAccent.shade,    -   -
+          child: Center(                          -   -   -   =  ** Container ın tamamını renklendirip ortalıyor.
+            child: Text(                          -   -   -   =   ** Yukarıdaki kod yazı kadar container yapıp renklendiriyor.
+              "Flutter'ı Seviyorum!",             -   -
+              style: TextStyle(fontSize: 30),     -
+            ),
+          )),*/
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  );
 }
